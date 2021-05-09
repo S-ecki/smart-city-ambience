9aaa2cca66b7c7ee0312a10bc0a45c0c04234616
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-05-09 21:17:29 +0200

    setup

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -6,8 +6,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("Home Screen comes here"),
+      // ScrollView (child) takes all the space it needs
+      width: double.infinity,
+      height: double.infinity,
+      // background color
+      color: Theme.of(context).backgroundColor,
+      // this makes the column scrollable -> prevents pixel overflow on smaller phones
+      child: SingleChildScrollView(
+        // distributing padding between outer container (here) and cards makes
+        // sure that padding on the edges and in between cards is the same
+        // resulting padding = 12
+        child: Padding(
+          padding: const EdgeInsets.all(6),
+          child: Column(children: [
+            Card(child: Text("hi"),),
+            Card(child: Text("hi"),),
+          ],),
+        ),
       ),
     );
   }
