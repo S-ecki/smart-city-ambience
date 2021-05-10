e63565a2d780a1d35a684275cd0f0313976052fd
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-05-10 02:00:27 +0200

    layout of homescreen + themeing

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smart_city_ambience/screens/home/emotion_input.dart';
+import 'package:smart_city_ambience/screens/home/emotion_output_tabs.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -11,19 +13,26 @@ class HomeScreen extends StatelessWidget {
       height: double.infinity,
       // background color
       color: Theme.of(context).backgroundColor,
-      // this makes the column scrollable -> prevents pixel overflow on smaller phones
-      child: SingleChildScrollView(
-        // distributing padding between outer container (here) and cards makes
-        // sure that padding on the edges and in between cards is the same
-        // resulting padding = 12
-        child: Padding(
+      child: Padding(
           padding: const EdgeInsets.all(6),
-          child: Column(children: [
-            Card(child: Text("hi"),),
-            Card(child: Text("hi"),),
-          ],),
-        ),
-      ),
+          child: LayoutBuilder(
+            builder: (context, dimens) {
+              return Column(
+                children: [
+                  SizedBox(
+                    height: dimens.maxHeight * 0.2,
+                    width: dimens.maxWidth,
+                    child: EmotionInput(),
+                  ),
+                  SizedBox(
+                    height: dimens.maxHeight * 0.8,
+                    width: dimens.maxWidth,
+                    child: EmotionOutputTabs(),
+                  ),
+                ],
+              );
+            },
+          )),
     );
   }
 }
