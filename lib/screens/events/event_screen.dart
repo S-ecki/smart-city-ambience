d00f77324486b653848984cac260670fd040cec3
samuelm00 <sa.mitterrutzner@gmail.com>
2021-05-15 10:42:17 +0200

    code refacotring

--- lib/screens/events/event_screen.dart
+++ lib/screens/events/event_screen.dart
@@ -16,85 +16,26 @@ class EventScreen extends StatelessWidget {
       // background color
       color: Theme.of(context).backgroundColor,
       // this makes the column scrollable -> prevents pixel overflow on smaller phones
-      child: SingleChildScrollView(
-        // distributing padding between outer container (here) and cards makes
-        // sure that padding on the edges and in between cards is the same
-        // resulting padding = 12
-        child: Padding(
-          padding: const EdgeInsets.all(6),
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed(SmortRoutes.eventDetailScreen,
-                      arguments: dummyEvents[0]);
-                },
-                child: EventCard(
-                  child: dummyEvents[0],
-                  showFullDesc: false,
-                  withBorder: true,
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed(SmortRoutes.eventDetailScreen,
-                      arguments: dummyEvents[1]);
-                },
-                child: EventCard(
-                  child: dummyEvents[1],
-                  showFullDesc: false,
-                  withBorder: true,
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed(SmortRoutes.eventDetailScreen,
-                      arguments: dummyEvents[2]);
-                },
-                child: EventCard(
-                  child: dummyEvents[2],
-                  showFullDesc: false,
-                  withBorder: true,
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed(SmortRoutes.eventDetailScreen,
-                      arguments: dummyEvents[3]);
-                },
-                child: EventCard(
-                  child: dummyEvents[3],
-                  showFullDesc: false,
-                  withBorder: true,
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed(SmortRoutes.eventDetailScreen,
-                      arguments: dummyEvents[4]);
-                },
-                child: EventCard(
-                  child: dummyEvents[4],
-                  showFullDesc: false,
-                  withBorder: true,
-                ),
-              ),
-                            InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed(SmortRoutes.eventDetailScreen,
-                      arguments: dummyEvents[5]);
-                },
-                child: EventCard(
-                  child: dummyEvents[5],
-                  showFullDesc: false,
-                  withBorder: true,
-                ),
-              ),
-            ],
+      child: Padding(
+        padding: const EdgeInsets.all(6),
+        child: ListView.separated(
+          itemCount: dummyEvents.length,
+          separatorBuilder: (context, index) => SizedBox(
+            height: 20,
+          ),
+          itemBuilder: (BuildContext context, int index) => InkWell(
+            onTap: () {
+              Navigator.of(context).pushNamed(SmortRoutes.eventDetailScreen,
+                  arguments: dummyEvents[index]);
+            },
+            child: EventCard(
+              child: dummyEvents[index],
+              showFullDesc: false,
+              withBorder: true,
+            ),
           ),
         ),
       ),
     );
   }
 }
-  
