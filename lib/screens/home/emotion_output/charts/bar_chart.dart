6dd19231debd6ea1d00386048cef80808227b8aa
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-05-15 13:40:03 +0200

    Piechart updates with redux

--- lib/screens/home/emotion_output/charts/bar_chart.dart
+++ lib/screens/home/emotion_output/charts/bar_chart.dart
@@ -4,17 +4,8 @@ import 'package:smart_city_ambience/redux/reactionsState.dart';
 import 'package:smart_city_ambience/types/enahancedEmoji.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
-class ChartData {}
-
 class BarChart extends StatelessWidget {
-  final List<BarChartData> chartData = <BarChartData>[
-    BarChartData("Februar", 143, 37, 61),
-    BarChartData("März", 83, 12, 60),
-    BarChartData("April", 101, 41, 44),
-    BarChartData("Mai", 53, 9, 36),
-  ];
-
-  //counts the types of emojis
+  // counts the types of emojis
   // simply pass in the Map that you get from the redux store with the type
   int getEmojiCount(
       EmojiType type, Map<String, List<EnhancedEmoji>> enhancedEmojis) {
@@ -43,9 +34,6 @@ class BarChart extends StatelessWidget {
         ),
         primaryXAxis: CategoryAxis(
           edgeLabelPlacement: EdgeLabelPlacement.shift,
-
-          // intervalType: DateTimeIntervalType.months,
-          // dateFormat: DateFormat.MMMM(),
           // show all labels
           interval: 1,
           // smart alignment of labels if the intersect each other
@@ -66,13 +54,23 @@ class BarChart extends StatelessWidget {
     );
   }
 
-  /// Returns the list of chart serie which need to render
-  /// on the stacked column chart.
+  /// Returns the list of chart serie which needs to render
+  /// on the stacked column chart
   List<StackedColumnSeries<BarChartData, String>> _getStackedColumnSeries(
       Map<String, List<EnhancedEmoji>> enhancedEmojis) {
-    // Example
-    print(getEmojiCount(EmojiType.Negative, enhancedEmojis));
-    //
+
+    // extract all set emojis from app
+    int positiveMay = getEmojiCount(EmojiType.Positive, enhancedEmojis);
+    int neutralMay = getEmojiCount(EmojiType.Neutral, enhancedEmojis);
+    int negativeMay = getEmojiCount(EmojiType.Negative, enhancedEmojis);
+
+    // add to dummy data
+    final List<BarChartData> chartData = <BarChartData>[
+      BarChartData("Februar", 143, 37, 61),
+      BarChartData("März", 83, 12, 60),
+      BarChartData("April", 101, 41, 44),
+      BarChartData("Mai", 53 + positiveMay, 9 + neutralMay, 36 + negativeMay),
+    ];
 
     return <StackedColumnSeries<BarChartData, String>>[
       StackedColumnSeries<BarChartData, String>(
