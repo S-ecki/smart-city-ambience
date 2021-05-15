6838e5ad4a87b0edee71ced3ad794595275039ba
Alexander Stadler <a01427369@unet.univie.ac.at>
2021-05-15 10:59:16 +0200

    added dummy data

--- lib/dummyData/dummyEvents.dart
+++ lib/dummyData/dummyEvents.dart
@@ -3,12 +3,32 @@ import 'package:smart_city_ambience/screens/events/event_card.dart';
 List<Event> dummyEvents = [
   Event(
       image: 'lib/images/test_image.jpg',
-      title: "test",
+      title: "Gewürzmarkt am Hauptplatz",
       description:
-          "this is an event adlösfalö sdlöfkaöl alösdlfalösk with a lot of text aklsdfjakls aklösdfj kljaaklösdfjak ljaklösdfakls jalösdkfaklösdj aklösddfj kla aklösdfjaklösdjfkjl aklösddf klasdjflaköks aklösdfj klöasdjf klasjsfaklöjadslk jafklsdjfalks j adlksfj"),
+          "Wie wir alle wissen haben verschiedene Gewürze Vorteile für unsere Gesundheit. Man nehme die örtlich verkauften Gewürze, wie Mayoran, Kümmel, Zimt, Safran und Chilli. Bei unserem Stand am Hauptplatz können Sie die verschiedensten gewürze erwerben und weitere Informationen zu deren gesundheitlichen Vorteilen erfahren."),
   Event(
-      image: 'lib/images/test_image.jpg',
-      title: "test",
+      image: 'lib/images/yoga.jpg',
+      title: "Gemeinsame Yogaklasse im Park",
       description:
-          "this is an event adlösfalö sdlöfkaöl ajkladlfjködfaksjlöaksdfjlöasdfkjlöjklöadfgfsjioöadfsöjkladfsljköadfskjlöaksdfjlöasdfkjlöaksdfjlöajklödsfjklödfsjklödafsjklöadsfjklöasdföjkldafsöjklasdfjklöasdfjklösjklöasdfjklöadfsjkladfsjklödafsjklödafsöjkldafsjöklasdfjklöasdföjkllösdlfalösk with a lot of text aklsdfjakls aklösdfj kljaaklösdfjak ljaklösdfakls jalösdkfaklösdj aklösddfj kla aklösdfjaklösdjfkjl aklösddf klasdjflaköks aklösdfj klöasdjf klasjsfaklöjadslk jafklsdjfalks j adlksfj"),
+          "Frau Stanizl veranstaltet jeden Samstag und Sonntag um 10:00 bei Schönwetter eine Yoga-Klasse im Park. An der ersten Stunde können Sie gratis und ohne Voranmeldung teilnehmen. Nähere Informationen finden Sie unter yoga-im-park.at"),
+  Event(
+      image: 'lib/images/hund.jpg',
+      title: "Hundewandertag",
+      description:
+          "Jährlich findet am ersten Sonntag im Juni der Hundewandertag des örtlichen Tierquartiers statt. Teilnehmen kann jeder, mit eigenem Hund oder einem \"Leihund\" aus dem Tierquartier. Über zahlreiche Teilnehmer freut sich wie jedes Jahr das Tierquartier, sowie dessen Bewohner. Start ist um 14:00 beim Tierquartier."),
+  Event(
+      image: 'lib/images/bibliothek.jpg',
+      title: "Kommen Sie, lesen Sie!",
+      description:
+          "Die Bibliothek hat nach dem einjährigen Umbau wieder mit mehr Lesestoff als je zuvor eröffnet. Scheuen Sie sich nicht und schauen vorbei. Am Eröffnungstag, dem 10.Juni, gibt es ganztägig Getränke und kleine Häppchen. Sehen Sie sich auch den ganz neuen Lesesaal an, der an Gemütlichkeit nicht zu übetreffen ist."),
+  Event(
+      image: 'lib/images/gastronomie.jpg',
+      title: "Gastronomie Wiedereröffnung",
+      description:
+          "Am 19. Mai ist es endlich soweit. Die lokale Gastronomie öffnet endlich wieder die Innen- und Außenbereiche für Kunden. Reservieren Sie rechtzeitig einen Tisch für einen entspannten Abend. Gemäß der Bundesverordnung, muss ein negativer Test, oder eine bereits erhaltene Impfung vorgezeigt werden."),
+  Event(
+      image: 'lib/images/storch.jpg',
+      title: "Störche gesichtet",
+      description:
+          "Am Dach unserer Apotheke hat sich letzte Woche Storchennachwuchs angekündigt. Wir freuen uns über das Storchenbaby Paula. Wir bitten Sie die Störche nicht zu füttern und genügend Abstand zu halten. "),
 ];
--- lib/dummyData/dummyForumEntries.dart
+++ lib/dummyData/dummyForumEntries.dart
@@ -1,8 +1,8 @@
 import 'package:smart_city_ambience/screens/chats/forum_card.dart';
 
 List<Forum> dummyForumEntries = [
-  Forum(title: "Simon", description: "A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text A lot of text "),
-  Forum(title: "Samuel", description: "A lot of text A lot of text A lot of text A lot"),
-  Forum(title: "Manuel", description: "A lot of text A lot of text A lot of text A lot"),
-  Forum(title: "Alex", description: "A lot of text A lot of text A lot of text A lot"),
+  Forum(title: "Grillparty", description: "Am Sonntag soll das Wetter wunderschön werden. Hätte sonst noch jemand Interesse daran, den Grillplatz zu mieten und gemeinsam zu grillen? LG Alex"),
+  Forum(title: "Was haltet ihr von dem Musikfestival in unserer Veranstaltungshalle?", description: "Ich habe Bedenken, dass das Event zu laut ist und am nächsten Tag viel Müll herumliegt. Was haltet ihr davon? LG Karen"),
+  Forum(title: "Regionale Rezeptideen", description: "Am Wochenende kommen meine Schwiegereltern zu Besuch. Ich würde gerne selbst kochen und zwar ein typisches Gericht. Hat jemand von euch gute Ideen? LG Peter"),
+  Forum(title: "Ich verschenke eine Kinderschaukel", description: "Da meine Kinder langsam zu alt für die Schaukel geworden sind, verschenke ich die Schaukel. Bei Interessen einfach ein Kommentar hinterlassen. LG Paul"),
 ];
