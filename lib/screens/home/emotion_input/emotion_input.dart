c79aa6d9e1c82c99c21eca75e0d0474d53a59729
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-05-15 12:29:44 +0200

    Refactoring

--- lib/screens/home/emotion_input/emotion_input.dart
+++ lib/screens/home/emotion_input/emotion_input.dart
@@ -1,13 +1,13 @@
 import 'package:customtogglebuttons/customtogglebuttons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:smart_city_ambience/emojiReactList/emojiReactList.dart';
-import 'package:smart_city_ambience/emojiReactPicker/emojiReactPicker.dart';
 import 'package:smart_city_ambience/redux/reactionsState.actions.dart';
 import 'package:smart_city_ambience/redux/reactionsState.dart';
 import 'package:smart_city_ambience/screens/events/event_card.dart';
 import 'package:smart_city_ambience/screens/home/emotion_input/emoji_button_home.dart';
 import 'package:smart_city_ambience/types/enahancedEmoji.dart';
+
+import 'home_text_field.dart';
 
 class EmotionInput extends StatefulWidget {
   const EmotionInput({Key key}) : super(key: key);
@@ -17,38 +17,46 @@ class EmotionInput extends StatefulWidget {
 }
 
 class _EmotionInputState extends State<EmotionInput> {
-  Event homeEvent = Event();
-  List<bool> _isSelected = [for (var _ in emojiReactionList) false];
-  FocusNode _focusNode = FocusNode();
+  // Event homeEvent = Event(); // used to declare a home-event in redux
   TextEditingController _controller = TextEditingController();
+  FocusNode _focusNode = FocusNode();
+
+  // used for ToggleButtons
+  // initially filled with 12x false - all 12 emojis are not selected
+  List<bool> _isSelected;
+
+  @override
+  void initState() {
+    super.initState();
+    _isSelected = [for (var _ in emojiReactionList) false];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: EdgeInsets.all(4),
+        padding: EdgeInsets.fromLTRB(8, 10, 8, 8),
         child: Row(
           children: [
+            // emoji toogle buttons
             Expanded(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   CustomToggleButtons(
-                    spacing: 0,
-                    runSpacing: 0,
                     constraints: BoxConstraints(
-                      maxHeight: 100,
-                      maxWidth: 100,
+                      maxHeight: 45,
+                      maxWidth: 45,
                     ),
                     fillColor: Colors.grey[200],
                     renderBorder: false,
                     isSelected: _isSelected,
+                    // fill with all emojis
                     children: <Widget>[
                       for (var emoji in emojiReactionList)
                         EmojiButtonHome(enhancedEmoji: emoji),
                     ],
+                    // alter list<bool> to select emojis
                     onPressed: (index) {
                       setState(
                         () {
@@ -61,60 +69,62 @@ class _EmotionInputState extends State<EmotionInput> {
               ),
             ),
             Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextFormField(
-                    focusNode: _focusNode,
-                    controller: _controller,
-                    maxLines: 2,
-                    decoration: InputDecoration(
-                      hintText: "Verbalisiere deine heutigen Gefühle",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text("Eingabe speichern"),
-                      IconButton(
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    // textfield
+                    HomeTextField(
+                        focusNode: _focusNode, controller: _controller),
+                    Spacer(),
+                    // save button
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Eingabe speichern"),
+                        IconButton(
+                          constraints: BoxConstraints(maxHeight: 35),
+                          iconSize: 20,
                           icon: Icon(Icons.save),
-                          onPressed: () {
-                            // clear input and unfocus
-                            _controller.clear();
-                            _focusNode.unfocus();
-                            // let user know it was saved
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text("Gefühl gespeichert."),
-                              ),
-                            );
-                            for (int i = 0; i < 12; ++i) {
-                              // add to emotion store
-                              if (_isSelected[i]) {
-                                StoreProvider.of<ReactionsState>(context)
-                                    .dispatch(
-                                  AddReaction(
-                                      enhancedEmoji: emojiReactionList[i],
-                                      eventId: "home"),
-                                );
-                              }
-                              // reset emojis
-                              _isSelected[i] = false;
-                            }
-                            // update UI
-                            setState(
-                              () {},
-                            );
-                          })
-                    ],
-                  )
-                ],
+                          onPressed: () => _onPressed(),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _onPressed() {
+    // TODO: add words provider
+    // clear input and unfocus
+    _controller.clear();
+    _focusNode.unfocus();
+    // let user know it was saved
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Gefühl gespeichert."),
+      ),
+    );
+    for (int i = 0; i < 12; ++i) {
+      // add to emotion store
+      // TODO: works?
+      if (_isSelected[i]) {
+        StoreProvider.of<ReactionsState>(context).dispatch(
+          AddReaction(enhancedEmoji: emojiReactionList[i], eventId: "home"),
+        );
+      }
+      // reset emojis
+      _isSelected[i] = false;
+    }
+    // update UI
+    setState(
+      () {},
     );
   }
 }
