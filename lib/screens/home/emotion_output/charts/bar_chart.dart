bd02e417b2568767c6d5d83a35b5a529901f9b99
samuelm00 <sa.mitterrutzner@gmail.com>
2021-05-15 11:22:22 +0200

    implementing emoji count

--- lib/screens/home/emotion_output/charts/bar_chart.dart
+++ lib/screens/home/emotion_output/charts/bar_chart.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:smart_city_ambience/redux/reactionsState.dart';
+import 'package:smart_city_ambience/types/enahancedEmoji.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ChartData {}
 
 class BarChart extends StatelessWidget {
-
-  // TODO: get from redux
-  int negativeMay = 0;
-  int neutralMay = 0;
-  int positiveMay = 0;
-
   final List<BarChartData> chartData = <BarChartData>[
     BarChartData("Februar", 143, 37, 61),
     BarChartData("März", 83, 12, 60),
@@ -17,42 +14,66 @@ class BarChart extends StatelessWidget {
     BarChartData("Mai", 53, 9, 36),
   ];
 
-  Widget build(BuildContext context) {
-    return SfCartesianChart(
-      plotAreaBorderWidth: 0,
-      title: ChartTitle(text: 'Some Title'),
-      legend: Legend(
-        isVisible: true,
-        overflowMode: LegendItemOverflowMode.wrap,
-        position: LegendPosition.bottom,
-      ),
-      primaryXAxis: CategoryAxis(
-        edgeLabelPlacement: EdgeLabelPlacement.shift,
+  //counts the types of emojis
+  // simply pass in the Map that you get from the redux store with the type
+  int getEmojiCount(
+      EmojiType type, Map<String, List<EnhancedEmoji>> enhancedEmojis) {
+    List<List<EnhancedEmoji>> entryList =
+        enhancedEmojis.entries.map((mapEntry) => mapEntry.value).toList();
+    int count = 0;
+    entryList.forEach((list) {
+      list.forEach((element) {
+        if (element.type == type) count++;
+      });
+    });
+    return count;
+  }
 
-        // intervalType: DateTimeIntervalType.months,
-        // dateFormat: DateFormat.MMMM(),
-        // show all labels
-        interval: 1,
-        // smart alignment of labels if the intersect each other
-        labelIntersectAction: AxisLabelIntersectAction.multipleRows,
-        // to disable vertical lines
-        majorGridLines: MajorGridLines(width: 0),
-        // caps the number of labels per 100 px
+  Widget build(BuildContext context) {
+    return StoreConnector<ReactionsState, Map<String, List<EnhancedEmoji>>>(
+      converter: (store) => store.state.enhancedEmojis,
+      builder: (context, Map<String, List<EnhancedEmoji>> enhancedEmojis) =>
+          SfCartesianChart(
+        plotAreaBorderWidth: 0,
+        title: ChartTitle(text: 'Some Title'),
+        legend: Legend(
+          isVisible: true,
+          overflowMode: LegendItemOverflowMode.wrap,
+          position: LegendPosition.bottom,
+        ),
+        primaryXAxis: CategoryAxis(
+          edgeLabelPlacement: EdgeLabelPlacement.shift,
+
+          // intervalType: DateTimeIntervalType.months,
+          // dateFormat: DateFormat.MMMM(),
+          // show all labels
+          interval: 1,
+          // smart alignment of labels if the intersect each other
+          labelIntersectAction: AxisLabelIntersectAction.multipleRows,
+          // to disable vertical lines
+          majorGridLines: MajorGridLines(width: 0),
+          // caps the number of labels per 100 px
+        ),
+        primaryYAxis: NumericAxis(
+          axisLine: AxisLine(width: 0),
+          // labelFormat: '{value}',
+          // maximum: 300,
+          majorTickLines: MajorTickLines(size: 0),
+        ),
+        series: _getStackedColumnSeries(enhancedEmojis),
+        tooltipBehavior: TooltipBehavior(enable: true),
       ),
-      primaryYAxis: NumericAxis(
-        axisLine: AxisLine(width: 0),
-        // labelFormat: '{value}',
-        // maximum: 300,
-        majorTickLines: MajorTickLines(size: 0),
-      ),
-      series: _getStackedColumnSeries(),
-      tooltipBehavior: TooltipBehavior(enable: true),
     );
   }
 
   /// Returns the list of chart serie which need to render
   /// on the stacked column chart.
-  List<StackedColumnSeries<BarChartData, String>> _getStackedColumnSeries() {
+  List<StackedColumnSeries<BarChartData, String>> _getStackedColumnSeries(
+      Map<String, List<EnhancedEmoji>> enhancedEmojis) {
+    // Example
+    print(getEmojiCount(EmojiType.Negative, enhancedEmojis));
+    //
+
     return <StackedColumnSeries<BarChartData, String>>[
       StackedColumnSeries<BarChartData, String>(
         dataSource: chartData,
