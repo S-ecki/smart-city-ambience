8ca70ac6fa6cd9a1f67c394b76dd7a9c63f7f446
S-ecki <75510543+S-ecki@users.noreply.github.com>
2021-05-15 19:19:50 +0200

    Refactoring

--- lib/screens/home/emotion_output/emotion_output_tabs.dart
+++ lib/screens/home/emotion_output/emotion_output_tabs.dart
@@ -1,11 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_scatter/flutter_scatter.dart';
-import 'package:provider/provider.dart';
 import 'package:smart_city_ambience/screens/home/emotion_output/charts/bar_chart.dart';
 import 'package:smart_city_ambience/screens/home/emotion_output/charts/pie_chart.dart';
-import 'package:smart_city_ambience/types/word_cloud.dart';
+import 'package:smart_city_ambience/screens/home/emotion_output/word_cloud.dart';
 
 class EmotionOutputTabs extends StatelessWidget {
   const EmotionOutputTabs({Key key}) : super(key: key);
@@ -76,38 +72,4 @@ class EmotionOutputTabs extends StatelessWidget {
   }
 }
 
-// // onyl to get random test strings
 
-// const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
-// Random _rnd = Random();
-
-// String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
-//     length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
-
-// List<String> test = [for (int i = 0; i < 50; ++i) getRandomString(6)];
-
-// // small test of package
-
-class WordCloudExample extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    List<String> wordList = Provider.of<WordCloud>(context).words;
-    List<Widget> widgets = <Widget>[];
-    for (var i = 0; i < wordList.length; i++) {
-      widgets.add(Text(wordList[i]));
-    }
-
-    final screenSize = MediaQuery.of(context).size;
-    final ratio = screenSize.width / screenSize.height;
-
-    return Center(
-      child: FittedBox(
-        child: Scatter(
-          fillGaps: true,
-          delegate: ArchimedeanSpiralScatterDelegate(ratio: ratio),
-          children: widgets,
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/screens/home/emotion_output/word_cloud.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_scatter/flutter_scatter.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_city_ambience/types/word_cloud.dart';
+
+class WordCloudExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<String> wordList = Provider.of<WordCloud>(context).words;
+    List<Widget> widgets = <Widget>[];
+    for (var i = 0; i < wordList.length; i++) {
+      widgets.add(Text(wordList[i]));
+    }
+
+    final screenSize = MediaQuery.of(context).size;
+    final ratio = screenSize.width / screenSize.height;
+
+    return Center(
+      child: FittedBox(
+        child: Scatter(
+          fillGaps: true,
+          delegate: ArchimedeanSpiralScatterDelegate(ratio: ratio),
+          children: widgets,
+        ),
+      ),
+    );
+  }
+}
