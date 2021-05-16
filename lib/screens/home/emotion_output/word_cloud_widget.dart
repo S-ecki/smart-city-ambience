2bb59d962a7e65669de90c0163ae5ae89a07b3dc
Alexander Stadler <a01427369@unet.univie.ac.at>
2021-05-16 14:09:11 +0200

    1

--- lib/screens/home/emotion_output/word_cloud_widget.dart
+++ lib/screens/home/emotion_output/word_cloud_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_scatter/flutter_scatter.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_city_ambience/types/word_cloud.dart';
+import 'package:hashtagable/hashtagable.dart';
 
 class WordCloudWidget extends StatelessWidget {
   @override
@@ -24,6 +25,28 @@ class WordCloudWidget extends StatelessWidget {
           delegate: ArchimedeanSpiralScatterDelegate(ratio: ratio),
           children: widgets,
         ),
+      ),
+    );
+  }
+}
+
+
+class ScatterItem extends StatelessWidget {
+  ScatterItem(this.hashtag, this.index);
+  final HashTagText hashtag;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle style = Theme.of(context).textTheme.body1.copyWith(
+          fontSize: hashtag.size.toDouble(),
+          color: hashtag.color,
+        );
+    return RotatedBox(
+      quarterTurns: hashtag.rotated ? 1 : 0,
+      child: Text(
+        hashtag.hashtag,
+        style: style,
       ),
     );
   }
